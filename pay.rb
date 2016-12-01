408ff235cf9de635275a5ad50e07e3d53e30dc3f
Michael Bianco <iloveitaly@gmail.com>
2016-12-01 14:24:25 -0700

    Adding ACH bank transfer payment support

--- pay.rb
+++ pay.rb
@@ -6,8 +6,15 @@ require "sinatra/reloader" if development?
 
 require 'stripe'
 require 'netsuite'
+require 'plaid'
 
 require 'pry'
+
+Plaid.config do |p|
+  p.client_id = ENV['PLAID_CLIENT_ID']
+  p.secret = ENV['PLAID_SECRET']
+  p.env = (development?) ? :tartan : :production
+end
 
 Stripe.api_key = ENV['STRIPE_KEY']
 
@@ -36,6 +43,7 @@ get '/' do
   redirect "https://github.com/iloveitaly/netsuite_invoice_payment_with_stripe"
 end
 
+# this is helpful for finding invoices in your sandbox to pay off
 get '/random' do
   open_invoices = NetSuite::Utilities.backoff { NetSuite::Records::Invoice.search(
     basic: [
@@ -81,12 +89,13 @@ get '/:invoice_id' do
     stripe_customer_id = nil
   end
 
-  invoice_total_in_cents = (ns_invoice.total.to_f * 100.0).to_i
+  invoice_total = BigDecimal.new(ns_invoice.total)
+  invoice_total_in_cents = (invoice_total * 100.0).to_i
 
   erb :pay, locals: {
     invoice_number: ns_invoice.tran_id,
     invoice_id: ns_invoice.internal_id,
-    invoice_total: (ns_invoice.total.to_f * 100.0).to_i,
+    invoice_total: invoice_total_in_cents,
     invoice_due_date: ns_invoice.due_date,
 
     stripe_customer_id: stripe_customer_id,
@@ -95,21 +104,44 @@ get '/:invoice_id' do
 end
 
 post '/pay' do
-  token = params[:stripeToken]
   invoice_total = params[:invoice_total]
   invoice_number = params[:invoice_number]
+  invoice_id = params[:invoice_id]
   customer_email = params[:customer_email]
 
-  # the token is used to create a one-time charge
+  # the token, either a card or bank account token, is used to create a one-time charge
+
+  stripe_checkout_token = params[:stripeToken]
+
+  plaid_account_id = params[:plaid_account_id]
+  plaid_account_token = params[:plaid_account_token]
+
+  if !plaid_account_id.nil?
+    plaid_user = Plaid::User.exchange_token(
+      plaid_account_token,
+      plaid_account_id,
+    )
+
+    stripe_token = plaid_user.stripe_bank_account_token
+
+    # token is nil if plaid + stripe tokens are not linked correctly
+    if stripe_token.nil?
+      fail "error retrieving bank token from plaid"
+    end
+  elsif !stripe_checkout_token.nil?
+    stripe_token = stripe_checkout_token
+  else
+    fail "no token specified"
+  end
 
   # instead of creating a one-time payment, you can add the card as a payment
   # source to the associated customer for easy reuse in the future
 
   begin
-    charge = Stripe::Charge.create(
+    charge = Stripe::Charge.create({
       amount: invoice_total,
       currency: 'usd',
-      source: token,
+      source: stripe_token,
 
       # this description will be added to the memo field of the NetSuite customer payment
       description: "Online NetSuite Invoice Payment #{invoice_number}",
@@ -118,11 +150,11 @@ post '/pay' do
 
       metadata: {
         # this metadata field instructs SuiteSync to create a CustomerPayment and apply it to the associated invoice
-        netsuite_invoice_id: params[:invoice_id]
+        netsuite_invoice_id: invoice_id
 
-        # more metadata fields can be added to pass custom data over to the
+        # more metadata fields can be added to pass custom data over to the integration
       }
-    )
+    })
   rescue Stripe::CardError => e
     raise e
   end
